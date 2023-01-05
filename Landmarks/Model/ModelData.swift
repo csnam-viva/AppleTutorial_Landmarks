55c76af44931f3a4aa6641dfc1d547c8adb72d3c
csnam-apple <dontforgetcosmos@gmail.com>
2023-01-05 15:17:41 +0900

    section 4

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  Data.swift
+//  Landmarks
+//
+//  Created by 비바 on 2023/01/05.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T{
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Coudn't find \(filename) in main bundle")
+    }
+    do {
+        data = try Data(contentsOf:  file)
+    } catch {
+        fatalError("Coudn't find \(filename) in main bundle \n\(error)")
+    }
+    do {
+        let decorder = JSONDecoder()
+        return try  decorder.decode(T.self, from: data)
+    } catch  {
+        fatalError("Coudn't find \(filename) in main bundle as \(T.self) \n\(error)")
+    }
+    
+}
